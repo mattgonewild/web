f5c29687c9d08b983c9f6139909ba8e9beb2db27
Matt W <wiseman@mattgonewild.com>
2023-02-04 01:26:54 -0500

    user -> session model, dart:io -> dart:html

--- static/flutter_service_worker.js
+++ static/flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
-"main.dart.js": "517182c61020e1c8646ac37b938c871e",
+"main.dart.js": "c0732de6a08909717e8d59c35d6b35c3",
 "version.json": "3db7f3e838439134c9a41674ed91370a",
 "assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
@@ -20,8 +20,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "a1952b046da3c05e0be6cf144d2dc96e",
-"index.html": "09d87d9177502a7a71a70538cfdc9630",
-"/": "09d87d9177502a7a71a70538cfdc9630"
+"index.html": "ca99bba66f0a399c1e842dcefac3dd04",
+"/": "ca99bba66f0a399c1e842dcefac3dd04"
 };
 
 // The application shell files that are downloaded before a service worker can
